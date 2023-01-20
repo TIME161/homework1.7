a662246358c588078ad532ebe22c574636dbe020
TIME <opout777@yandex.ru>
2023-01-20 15:50:36 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -9,10 +10,25 @@ public class Main {
         int firstMonth = 1;
         int countInBank = 0;
         int payPerMont = 15000;
-        while  (countInBank < 2_549_000) {
+        while (countInBank < 2_549_000) {
             countInBank += countInBank / 100;
             countInBank += payPerMont;
             System.out.println("Месяц " + firstMonth++ + ", сумма накоплений равна " + countInBank + " рублей.");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        {int number = 1;
+        while (number <= 10) {
+            System.out.print(number + " ");
+            number++;}
+    }
+        {System.out.println(" ");}
+        for (int number = 10; number >= 1;) {
+            System.out.print( number + " ");
+            number--;
         }
     }
 }
